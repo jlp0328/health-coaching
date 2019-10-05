2194cfeecd0abc45721a54ab65e16b38ab076c29
jlp0328 <jlp0328@gmail.com>
2019-10-05 08:08:22 -0400

    Updated config file with plugin

--- gridsome.config.js
+++ gridsome.config.js
@@ -1,10 +1,24 @@
-// This is where project configuration and plugin options are located. 
+// This is where project configuration and plugin options are located.
 // Learn more: https://gridsome.org/docs/config
 
 // Changes here require a server restart.
 // To restart press CTRL + C in terminal and run `gridsome develop`
 
 module.exports = {
-  siteName: 'Gridsome',
-  plugins: []
-}
+  siteName: "health-coaching",
+  plugins: [
+    {
+      use: "@gridsome/source-wordpress",
+      options: {
+        baseUrl: process.env.WORDPRESS_URL, // required
+        apiBase: "wp-json",
+        typeName: "WordPress",
+        perPage: 100,
+        concurrent: 10
+      }
+    }
+  ],
+  templates: {
+    WordPressPost: "/:year/:month/:day/:slug"
+  }
+};
