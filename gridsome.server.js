5fe6a8c3728a108858ba9097f3cffa27a325dd70
jlp0328 <jlp0328@gmail.com>
2019-10-13 11:42:40 -0400

    comment out server file

--- gridsome.server.js
+++ gridsome.server.js
@@ -1,19 +1,21 @@
-const axios = require("axios");
+// const axios = require("axios");
 
-module.exports = function(api) {
-  api.loadSource(async actions => {
-    const { data } = await axios.get(
-      "https://health-db-9afe2.firebaseapp.com/api/v1/all-clients/"
-    );
+//Create JSON object/file with API, typeName, nodeName...
+//Loop over the object to create all of the endpoints
+// module.exports = function(api) {
+//   api.loadSource(async actions => {
+//     const { data } = await axios.get(
+//       "https://health-db-9afe2.firebaseapp.com/api/v1/clients/all-clients/"
+//     );
 
-    const collection = actions.addCollection({
-      typeName: "Clients"
-    });
+//     const collection = actions.addCollection({
+//       typeName: "Clients"
+//     });
 
-    data.clientList.forEach(client => {
-      collection.addNode({
-        client
-      });
-    });
-  });
-};
+//     data.clientList.forEach(client => {
+//       collection.addNode({
+//         client
+//       });
+//     });
+//   });
+// };
