527cbff4110483bd7b74bdcbde680c9b7bec1205
Pittman, Jamie <jamie.pittman@fmr.com>
2019-10-28 09:01:16 -0400

    Add page for macro logging

--- gridsome.config.js
+++ gridsome.config.js
@@ -1,54 +1,54 @@
-const path = require("path");
+const path = require('path');
 
 function addStyleResource(rule) {
   rule
-    .use("style-resource")
-    .loader("style-resources-loader")
+    .use('style-resource')
+    .loader('style-resources-loader')
     .options({
-      patterns: [path.resolve(__dirname, "./src/assets/*.scss")]
+      patterns: [path.resolve(__dirname, './src/assets/*.scss')]
     });
 }
 
 module.exports = {
-  siteName: "health-coaching",
+  siteName: 'health-coaching',
   plugins: [
-    {
-      use: "gridsome-source-firestore",
-      options: {
-        credentials: require("./health-db-9afe2-firebase-adminsdk-edpz3-348f7f78af.json"),
-        debug: true,
-        collections: [
-          {
-            ref: db => {
-              return db.collection("clients");
-            },
-            slug: "clients",
-            children: [
-              {
-                ref: (db, parentDoc) => {
-                  return parentDoc.ref.collection("dailyWeight");
-                }
-              },
-              {
-                ref: (db, parentDoc) => {
-                  return parentDoc.ref.collection("dailyMacros");
-                }
-              },
-              {
-                ref: (db, parentDoc) => {
-                  return parentDoc.ref.collection("monthlyMeasurements");
-                }
-              }
-            ]
-          }
-        ]
-      }
-    }
+    // {
+    //   use: "gridsome-source-firestore",
+    //   options: {
+    //     credentials: require("./health-db-9afe2-firebase-adminsdk-edpz3-348f7f78af.json"),
+    //     debug: true,
+    //     collections: [
+    //       {
+    //         ref: db => {
+    //           return db.collection("clients");
+    //         },
+    //         slug: "clients",
+    //         children: [
+    //           {
+    //             ref: (db, parentDoc) => {
+    //               return parentDoc.ref.collection("dailyWeight");
+    //             }
+    //           },
+    //           {
+    //             ref: (db, parentDoc) => {
+    //               return parentDoc.ref.collection("dailyMacros");
+    //             }
+    //           },
+    //           {
+    //             ref: (db, parentDoc) => {
+    //               return parentDoc.ref.collection("monthlyMeasurements");
+    //             }
+    //           }
+    //         ]
+    //       }
+    //     ]
+    //   }
+    // }
   ],
   chainWebpack: config => {
-    const types = ["vue-modules", "vue", "normal-modules", "normal"];
+    const types = ['vue-modules', 'vue', 'normal-modules', 'normal'];
     types.forEach(type => {
-      addStyleResource(config.module.rule("scss").oneOf(type));
+      addStyleResource(config.module.rule('scss').oneOf(type));
     });
   }
 };
---
+++ src/pages/Macros.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <Navigation />
+    <div class="main"></div>
+  </div>
+</template>
+
+<script>
+import Navigation from "../components/Navigation";
+
+export default {
+  components: {
+    Navigation
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
